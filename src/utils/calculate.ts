39411533111e3abf88ddb4f553824a35da8b7d7f
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-03-16 15:30:46 +1000

    fixed incorrect math

--- src/utils/calculate.ts
+++ src/utils/calculate.ts
@@ -1,6 +1,5 @@
 export const fitExponential = (xData: number[], yData: (number | null)[]) => {
   const x: number[] = []
-  const y: number[] = []
   const logY: number[] = []
   const xSqr: number[] = []
   const xLogY: number[] = []
@@ -11,21 +10,19 @@ export const fitExponential = (xData: number[], yData: (number | null)[]) => {
       continue
     }
     x.push(xValue)
-    y.push(yValue)
     logY.push(Math.log(yValue))
     xSqr.push(xValue ** 2)
     xLogY.push(xValue * Math.log(yValue))
   }
   const xSum = x.reduce((acc, val) => acc + val, 0)
-  const ySum = y.reduce((acc, val) => acc + val, 0)
   const logYSum = logY.reduce((acc, val) => acc + val, 0)
   const xSqrSum = xSqr.reduce((acc, val) => acc + val, 0)
   const xLogYSum = xLogY.reduce((acc, val) => acc + val, 0)
   const slope = (x.length * xLogYSum - xSum * logYSum) / (x.length * xSqrSum - xSum ** 2)
   const base = Math.exp(slope)
-  const xMean = xSum / x.length - x[0]
-  const yMean = ySum / y.length
-  const constant = yMean / base ** xMean
+  const xPoint = xSum / x.length - x[0]
+  const yPoint = Math.exp(logYSum / logY.length)
+  const constant = yPoint / base ** xPoint
   return { constant, base }
 }
 
