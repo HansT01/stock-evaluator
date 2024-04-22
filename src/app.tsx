58f3174501824d8f89b016b159f686008fd0226f
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-04-23 02:56:28 +1000

    set theme to slate

--- src/app.tsx
+++ src/app.tsx
@@ -8,7 +8,7 @@ const themes = ['slate', 'chocolate', 'orchid']
 
 export default function App() {
   return (
-    <div class={cn('dark:dark bg-background text-background-fg', themes[Math.floor(Math.random() * themes.length)])}>
+    <div class={cn('dark:dark bg-background text-background-fg', 'slate')}>
       <Router root={(props) => <Suspense>{props.children}</Suspense>}>
         <FileRoutes />
       </Router>
